4bda5d9dd5c0f6a6d5267c7f9d9c77ec9b1044bd
diamondburned <diamond@libdb.so>
2024-05-13 02:46:46 -0700

    Add Flake package

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "A very basic flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -16,18 +14,38 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        name = "diamond-user-guide";
+
+        mdbookPkgs = with pkgs; [
+          mdbook
+          mdbook-pagetoc
+        ];
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            self.formatter.${system}
-            nodePackages.prettier
-            languagetool
-
-            mdbook
-            mdbook-pagetoc
-          ];
+          inherit name;
+          packages =
+            with pkgs;
+            [
+              self.formatter.${system}
+              nodePackages.prettier
+              languagetool
+            ]
+            ++ mdbookPkgs;
         };
+
+        packages.default =
+          pkgs.runCommand name
+            {
+              src = ./.;
+              nativeBuildInputs = mdbookPkgs;
+            }
+            ''
+              mkdir -p $out
+              cd $src
+              mdbook build -d $out
+            '';
+
         formatter = pkgs.nixfmt-rfc-style;
       }
     );
