aff8e70cb6991b06922259128399aea6818c2003
diamondburned <diamond@libdb.so>
2024-07-12 16:35:22 +0700

    Add Markdown files and _docsfs.json

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    libdb.url = "github:diamondburned/libdb.so";
+    libdb.inputs.nixpkgs.follows = "nixpkgs";
+    libdb.inputs.flake-utils.follows = "flake-utils";
   };
 
   outputs =
@@ -9,41 +13,60 @@
       self,
       nixpkgs,
       flake-utils,
+      libdb,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
         name = "diamond-user-guide";
-
-        mdbookPkgs = with pkgs; [
-          mdbook
-          mdbook-pagetoc
-        ];
       in
       {
         devShells.default = pkgs.mkShell {
           inherit name;
-          packages =
-            with pkgs;
-            [
-              self.formatter.${system}
-              nodePackages.prettier
-              languagetool
-            ]
-            ++ mdbookPkgs;
+          packages = with pkgs; [
+            self.formatter.${system}
+            nodePackages.prettier
+            languagetool
+
+            mdbook
+            mdbook-pagetoc
+          ];
         };
 
         packages.default =
           pkgs.runCommand name
             {
               src = ./.;
-              nativeBuildInputs = mdbookPkgs;
+              nativeBuildInputs = with pkgs; [
+                bash
+                jq
+
+                mdbook
+                mdbook-pagetoc
+              ];
+
+              # Set this for jsonfs.
+              BASE_URL = "https://docs.0xd14.id/";
             }
             ''
+              set -x
               mkdir -p $out
               cd $src
+
+              (
+                # Generate a fs.json for our documentation.
+                # We'll only include Markdown files.
+                cp -r --no-preserve=ownership src /tmp/docs
+                cd /tmp/docs && bash ${libdb}/scripts/jsonfs . > /tmp/_docsfs.json
+              )
+
+              # Render Markdown to HTML
               mdbook build -d $out
+
+              # Keep the original Markdown files
+              cp -r --no-preserve=ownership src/* $out/
+              mv /tmp/_docsfs.json $out/
             '';
 
         formatter = pkgs.nixfmt-rfc-style;
