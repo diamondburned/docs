3a2620a84be2af08b342ba661375fd66c698269b
diamondburned <diamond@libdb.so>
2024-12-01 14:52:12 -0800

    Add countdown to Continue button

--- scripts/index.ts
+++ scripts/index.ts
@@ -18,35 +18,61 @@ function respondContentAlert(agree: boolean) {
   }
 }
 
+const el = <T extends HTMLElement = HTMLElement>(id: string, must = true) => {
+  const el = document.getElementById(id) as T;
+  if (must && !el) {
+    throw new Error(`Element with id "${id}" not found.`);
+  }
+  return el;
+};
+
 function apply() {
   encryption.apply();
   namevars.apply();
   tocsummary.apply();
   anchor.apply();
 
+  const contentAlert = document.getElementById("content-alert");
+  if (!contentAlert) {
+    throw new Error("Content alert not found. Not continuing out of safety.");
+  }
+
+  if (localStorage.getItem("content-alert")) {
+    contentAlert.remove();
+  } else {
+    const backBtn = el<HTMLButtonElement>("content-alert-back");
+    backBtn.addEventListener("click", () => respondContentAlert(false));
+
+    const continueBtn = el<HTMLButtonElement>("content-alert-continue");
+    continueBtn.addEventListener("click", () => respondContentAlert(true));
+    continueBtn.disabled = true;
+    setTimeout(() => {
+      continueBtn.disabled = false;
+    }, 5000);
+
+    let countdown = 5;
+    const continueCountdown = el<HTMLSpanElement>("content-alert-countdown");
+    const updateCountdown = () => {
+      continueCountdown.textContent = countdown > 0 ? `(${countdown})` : "";
+    };
+
+    updateCountdown();
+    const interval = setInterval(() => {
+      countdown--;
+      updateCountdown();
+      if (countdown <= 0) clearInterval(interval);
+    }, 1000);
+  }
+
+  addEventListener("scroll", () => {
+    document.body.classList.toggle("scrolled", globalThis.scrollY > 0);
+  });
+
   forAllElements({
     selector: ".require-javascript",
     apply: (element) => {
       element.classList.remove("require-javascript");
     },
-  });
-
-  const contentAlert = document.getElementById("content-alert");
-  if (contentAlert) {
-    if (localStorage.getItem("content-alert")) {
-      contentAlert.remove();
-    } else {
-      document
-        .getElementById("content-alert-back")!
-        .addEventListener("click", () => respondContentAlert(false));
-      document
-        .getElementById("content-alert-continue")!
-        .addEventListener("click", () => respondContentAlert(true));
-    }
-  }
-
-  addEventListener("scroll", () => {
-    document.body.classList.toggle("scrolled", globalThis.scrollY > 0);
   });
 }
 
